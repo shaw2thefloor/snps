e2c8ba568e580e5f4ffe3a34785a5b69a680f32f
Felix Shaw <felix.shaw@tgac.ac.uk>
2017-01-01 15:32:03 +0000

    added functionality for reading snps in from Excel speadsheet (presumed format to be supplied by PI)

--- lib/snps_util.py
+++ lib/snps_util.py
@@ -1,4 +1,31 @@
 from scrape import genotype, snp
+from xlrd import open_workbook
+
+
+def read_snp_excel_file(filename):
+    wb = open_workbook(filename)
+    sheet = wb.sheet_by_index(0)
+
+    genes = list()
+    rs_refs = list()
+    samples = list()
+
+    for i in range(0, sheet.nrows):
+        sample = ()
+        for j in range(1, sheet.ncols):
+            # check for gene names
+            if i == 0:
+                genes.append(sheet.cell(i, j).value)
+            # check for rs_ref
+            elif i == 1:
+                rs_refs.append(sheet.cell(i, j).value)
+            else:
+                sample = sample + (sheet.cell(i, j).value,)
+        if i > 1:
+            samples.append(sample)
+
+    return {'genes': genes, 'rs_refs': rs_refs, 'samples': samples}
+
 
 def snpedia_lookup(snp_data):
     s = snp_data['snp']
@@ -6,5 +33,3 @@ def snpedia_lookup(snp_data):
     resp = snp(s, v)
 
     return resp
-
-
--- test/test_sample.py
+++ test/test_sample.py
@@ -1,5 +1,4 @@
-from lib.snps_util import snpedia_lookup
-import json
+from lib.snps_util import snpedia_lookup, read_snp_excel_file
 
 
 # function to create mock snp data
@@ -7,16 +6,22 @@ import json
 def make_snps_data():
     d = list()
     d.append({'gene': 'ADIPOQ', 'snp': 'rs17300539', 'variant': 'GG'})
-    d.append({'gene': 'ANKK1', 'snp': 'rs1800497', 'variant':'TT'})
+    d.append({'gene': 'ANKK1', 'snp': 'rs1800497', 'variant': 'TT'})
     d.append({'gene': 'FTO', 'snp': 'rs11076022', 'variant': 'GG'})
     return d
+
+
+def test_excel_file_load():
+    d = read_snp_excel_file('test_data/ExampleSNPTable.xlsx')
+    assert 'genes' in d
+    assert 'rs_refs' in d
+    assert 'samples' in d
 
 
 def test_snpedia_lookup():
     data = make_snps_data()
     for t in data:
         output = snpedia_lookup(t)
-        assert output != ''
+        assert type(output) == type(dict())
+        assert len(output) > 0
         print("output is: " + str(output))
-        #json_version = json.loads(output)
-
